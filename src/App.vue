2316cdd33f77fc02ef2951ddf32280b47d0b6819
jang <lemona9201@gmail.com>
2024-03-29 15:55:32 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>App</h1>    
+    <button @click="increaseCount">버튼</button>
+    {{ count }}
+    <ChildComp :color="color" :bgColor="bgColor" />
+    <button @click="color='red'">change color</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, watch } from 'vue';
+import ChildComp from './components/ChildComp.vue';
+
+let count = ref(0);
+let color = ref('green');
+let bgColor = ref('yellow');
+
+onMounted(() => {
+  console.log('mounted');
+})
+
+const increaseCount = () => {
+  count.value++;
+}
+
+watch(color, () => {
+  console.log('color 변경');
+});
+</script>
+
+<style scoped>
+
+</style>
